cd981a10174e561456c2568b204cd6affd714aad
Tanmay Mandal <115643345+Tanmay226@users.noreply.github.com>
2022-12-30 09:45:11 +0530

    Add files via upload

---
+++ 03_weeknumber.js
@@ -0,0 +1,27 @@
+let weeknumber = 9;
+
+if (weeknumber == 1) {
+    console.log("Sunday");
+
+}
+else if (weeknumber == 2) {
+    console.log("monday");
+}
+else if (weeknumber == 3) {
+    console.log("tuesday");
+}
+else if (weeknumber == 4) {
+    console.log("wednesday");
+}
+else if (weeknumber == 5) {
+    console.log("Thursday");
+}
+else if (weeknumber == 6) {
+    console.log("Friday");
+}
+else if (weeknumber == 7) {
+    console.log("saturday");
+}
+else {
+    console.log("week number is invalid");
+}
